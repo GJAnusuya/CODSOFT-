7fb22aecdf94f88ae83d2b7b1c5b1caf7ab8a53c
GJAnusuya <142823013+GJAnusuya@users.noreply.github.com>
2023-11-28 21:23:05 +0530

    Add files via upload

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Student Grade Calculator");
+
+        // Input the number of subjects
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        // Input marks for each subject
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+            totalMarks += marks[i];
+        }
+
+        // Calculate average percentage
+        double averagePercentage = calculateAveragePercentage(totalMarks, numSubjects);
+
+        // Determine the letter grade
+        char grade = calculateGrade(averagePercentage);
+
+        // Display results
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        // Close the scanner
+        scanner.close();
+    }
+
+    // Method to calculate the average percentage
+    private static double calculateAveragePercentage(int totalMarks, int numSubjects) {
+        return (double) totalMarks / numSubjects;
+    }
+
+    // Method to determine the letter grade based on the average percentage
+    private static char calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return 'A';
+        } else if (averagePercentage >= 80) {
+            return 'B';
+        } else if (averagePercentage >= 70) {
+            return 'C';
+        } else if (averagePercentage >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+}
+
